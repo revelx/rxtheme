8e9d4d36fbc6daf673c37081bd6e26079978a274
Bob Rietveld <bob.rietveld@revelx.nl>
2019-06-19 15:24:15 +0200

    Added natuurmonumenten palette.

--- R/color.R
+++ R/color.R
@@ -11,7 +11,13 @@
                   `deep space sparkle`    = "#3D5A6C",
                   `green sheen`           = "#72A98F",
                   `kiwi`                  = "#8DE969",
-                   `maximum green yellow` = "#CBEF43" )
+                  `maximum green yellow` = "#CBEF43" ,
+                  `nm_blue` ="#01009a",
+                  `nm_green` = "#007866",
+                  `nm_green_light` = "#89b934",
+                  `nm_pink` = "#e2006a",
+                  `nm_blue_light` = "#00aec7"
+                  )
 
 # internal function to return colors
   rx_cols <- function(...) {
@@ -25,9 +31,10 @@
 
 # set palletes
   ## green pallete from https://coolors.co/433a3f-3d5a6c-72a98f-8de969-cbef43
-  rx_palettes <- list( `main`  = rx_cols( "yellow", "black","dark yellow","light grey","light yellow","grey" ),
-                       `marc`  = rx_cols( "light yellow", "yellow","light grey"),
-                       `green` = rx_cols ("dark puce", "deep space sparkle","green sheen","kiwi","maximum green yellow")
+  rx_palettes <- list( `main`                = rx_cols( "yellow", "black","dark yellow","light grey","light yellow","grey" ),
+                       `marc`                = rx_cols( "light yellow", "yellow","light grey"),
+                       `green`               = rx_cols ("dark puce", "deep space sparkle","green sheen","kiwi","maximum green yellow"),
+                       `natuurmonumenten`    = rx_cols("nm_blue","nm_green" ,"nm_green_light","nm_pink","nm_blue_light")
   )
 
 
